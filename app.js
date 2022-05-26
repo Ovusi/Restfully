1265b2598a7b7f7d5fe8e3320ddcd67c53f0953a
Nobert Jakpor <jakpornobert@gmail.com>
2022-05-26 19:47:38 +0100

    new

--- app.js
+++ app.js
@@ -3,39 +3,47 @@ const { use } = require("express/lib/router")
 const Users = require("./models/users")
 const app = express()
 
+app.use(express.json())
 const users_ = new Users
 
 app.get("/api/get_user", async (req, res) => {
-    if (!req) {
-        // if payload  is empty, return error code.
-        res.statusCode(400)
-    } else {
-        // else we query the db and return the data.
-        await users_.retreive(req).then((data) => {
-            res.send(data)
-        })
+    if (req.method === "GET") {
+        if (!req.body) {
+            // if payload  is empty, return error code.
+            res.statusCode(400)
+        } else {
+            // else we query the db and return the data.
+            await users_.retreive(req.body).then((data) => {
+                res.send(data)
+            })
+        }      
     }
 })
 
 app.get("/api/get_all_users", async (req, res) => {
+    if (req.method === "GET") {
     // return an array of user keys from the db.
     await users_.getAllKeys()
         .then((data) => {
             res.send(data)
-        })
+        })        
+    }
 })
 
 app.post("/api/add_user", async (req, res) => {
-    if (users_.getAllKeys().includes(req)) {
-        // check if key exists in the db.
-        // if it exists, return an error.
-        res.send("Already exists.")      
-    } else {
-        // add new user to the db.
-        await users_.add(req).then((data) => {
-            res.send(data)
-        })
+    if(req.method === "POST") {
+        if (users_.getAllKeys().includes(req)) {
+            // check if key exists in the db.
+            // if it exists, return an error.
+            res.send("Already exists.")      
+        } else {
+            // add new user to the db.
+            await users_.add(req).then((data) => {
+                res.send(data)
+            })
+        }
     }
+
 })
 
 app.listen(3000, () => console.log("Listening on port 3000..."))
