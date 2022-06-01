126c6a79f81515fdbf67243740070ccb9493845d
Nobert Jakpor <jakpornobert@gmail.com>
2022-06-01 12:17:25 +0100

    new

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.get("/api/get_all_users", async (req, res) => {
     await users_.getAllKeys()
         .then((data) => {
             res.send(data)
-        }).catch((err) => res.send(err))
+        }).catch((err) => {
+            res.send(err)
+        })
 })
 
 /**
